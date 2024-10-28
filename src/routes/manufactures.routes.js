4c13d92ea0d1ebbb4e0ae0a5d3aa6ce3f0d79432
David Gurning <davidgurning4@gmail.com>
2024-10-28 10:36:57 +0700

    updated

--- src/routes/manufactures.routes.js
+++ src/routes/manufactures.routes.js
@@ -1,30 +1,32 @@
-const express = require('express');
+const express = require("express");
+const { authorization } = require("../middlewares/auth.middlewares");
 const {
-    validateGetAll,
-    validateGetById,
-    validateCreate,
-    validateUpdate,
-    validateDelete,
-} = require('../middlewares/manufactures.middlewares');
+  validateGetAll,
+  validateGetById,
+  validateCreate,
+  validateUpdate,
+  validateDelete,
+} = require("../middlewares/manufactures.middlewares");
 const {
-    getAll,
-    getById,
-    create,
-    update,
-    delete: deleteManufacture,
-} = require('../controllers/manufactures.controllers');
+  getAll,
+  getById,
+  create,
+  update,
+  delete: deleteManufacture,
+} = require("../controllers/manufactures.controllers");
+const { adminRole, userRole } = require("../constants/auth");
 
 const router = express.Router();
 
 router
-    .route('/')
-    .get(validateGetAll, getAll)
-    .post(validateCreate, create);
+  .route("/")
+  .get(authorization(adminRole, userRole), validateGetAll, getAll)
+  .post(authorization(adminRole), validateCreate, create);
 
 router
-    .route('/:id')
-    .get(validateGetById, getById)
-    .put(validateUpdate, update)
-    .delete(validateDelete, deleteManufacture);
+  .route("/:id")
+  .get(authorization(adminRole, userRole), validateGetById, getById)
+  .put(authorization(adminRole), validateUpdate, update)
+  .delete(authorization(adminRole), validateDelete, deleteManufacture);
 
 module.exports = router;
--- src/routes/types.routes.js
+++ src/routes/types.routes.js
@@ -1,30 +1,33 @@
 const express = require("express");
+const { authorization } = require("../middlewares/auth.middlewares");
+
 const {
-    validateGetTypes,
-    validateGetTypeById,
-    validateDeleteTypeById,
-    validateCreateType,
-    validateUpdateType,
+  validateGetTypes,
+  validateGetTypeById,
+  validateDeleteTypeById,
+  validateCreateType,
+  validateUpdateType,
 } = require("../middlewares/types.middlewares");
 const {
-    getTypes,
-    getTypeById,
-    deleteTypeById,
-    createType,
-    updateType,
+  getTypes,
+  getTypeById,
+  deleteTypeById,
+  createType,
+  updateType,
 } = require("../controllers/types.controllers");
+const { adminRole, userRole } = require("../constants/auth");
 
 const router = express.Router();
 
 router
-    .route("/")
-    .get(validateGetTypes, getTypes)
-    .post(validateCreateType, createType);
+  .route("/")
+  .get(authorization(adminRole, userRole), validateGetTypes, getTypes)
+  .post(authorization(adminRole), validateCreateType, createType);
 
 router
-    .route("/:id")
-    .get(validateGetTypeById, getTypeById)
-    .put(validateUpdateType, updateType)
-    .delete(validateDeleteTypeById, deleteTypeById);
+  .route("/:id")
+  .get(authorization(adminRole, userRole), validateGetTypeById, getTypeById)
+  .put(authorization(adminRole), validateUpdateType, updateType)
+  .delete(authorization(adminRole), validateDeleteTypeById, deleteTypeById);
 
 module.exports = router;
